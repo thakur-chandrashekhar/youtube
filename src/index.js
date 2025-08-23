5b7585bca2957e7b340c17c9ab8276ae52b1bd30
chandrashekhar <shekhar534042@gmail.com>
2025-08-23 23:16:42 +0530

    connect to mongoatls

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv"
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./.env"
+    
+});
+
+console.log("Loaded ENV →", process.env); 
+console.log("PORT:", `"${process.env.PORT}"`);
+console.log("MONGO URI:", `"${process.env.MONGODB_URI}"`);
+
+ connectDB().then(() => {
+  console.log("📦 Database connection successful!");
+  });
+
+
+/*
+import express from "express"
+import connectDB from "./db";
+(async () =>{
+    try {
+        await mongoose.connect('${process.env.MONGODB_URI}/${ DB_NAME}')
+        app.on("error",(error)=>{
+            console.log("ERRR:", error);
+            throw error
+        })
+      app.listen(process.env.PORT,() =>{
+        console.log('APP is listening on port ${process.env.PORT}');
+      })
+    } catch (error) {
+        console.log("ERROR:", error) 
+        throw err
+    }
+})()
+    */
